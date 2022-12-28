5b99bea3d3b162da0377e2bc8bd5237828830e51
Dylan Palmboom <nofxboy1234@gmail.com>
2022-12-28 15:11:31 +1000

    Use Benchmark to test optimized version of bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'pry-byebug'
+require 'benchmark'
 
 def swap_items_in_array(items, index_a, index_b)
   items[index_a], items[index_b] = items[index_b], items[index_a]
@@ -24,6 +25,32 @@ def bubble_sort(items)
   items
 end
 
+def bubble_sort_2(items)
+  n = items.length
+  swapped = true
+  while swapped
+    swapped = false
+    for i in 1..(n - 1)
+      index_a = i - 1
+      index_b = i
+      next unless items[index_a] > items[index_b]
+
+      # binding.pry
+      swap_items_in_array(items, index_a, index_b)
+      swapped = true
+    end
+    n -= 1
+  end
+  items
+end
+
 # [4, 3, 78, 2, 0, 2]
-# [3, 4, 2, 0, 2, 78]
-p bubble_sort([4, 3, 78, 2, 0, 2])
+# [0, 2, 2, 3, 4, 78]
+
+# p bubble_sort([4, 3, 78, 2, 0, 2])
+# p bubble_sort_2([4, 3, 78, 2, 0, 2])
+
+Benchmark.bm do |x|
+  x.report('A') { bubble_sort([4, 3, 78, 2, 0, 2]) }
+  x.report('B') { bubble_sort_2([4, 3, 78, 2, 0, 2]) }
+end
