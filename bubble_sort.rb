15c559ba2e0b90d4e399a0e52a6bc100997af16f
Dylan Palmboom <nofxboy1234@gmail.com>
2022-12-30 10:48:49 +1000

    Add 2 optimization variations of bubble_sort

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -44,6 +44,24 @@ def bubble_sort_2(items)
   items
 end
 
+def bubble_sort_3(items)
+  # binding.pry
+  n = items.length
+  while n > 1
+    new_n = 0
+    for i in 1..(n - 1)
+      index_a = i - 1
+      index_b = i
+      next unless items[index_a] > items[index_b]
+
+      swap_items_in_array(items, index_a, index_b)
+      new_n = i
+    end
+    n = new_n
+  end
+  items
+end
+
 # [4, 3, 78, 2, 0, 2]
 # [0, 2, 2, 3, 4, 78]
 
@@ -51,6 +69,7 @@ end
 # p bubble_sort_2([4, 3, 78, 2, 0, 2])
 
 Benchmark.bm do |x|
-  x.report('A') { bubble_sort([4, 3, 78, 2, 0, 2]) }
-  x.report('B') { bubble_sort_2([4, 3, 78, 2, 0, 2]) }
+  x.report('A') { p bubble_sort([4, 3, 78, 2, 0, 2]) }
+  x.report('B') { p bubble_sort_2([4, 3, 78, 2, 0, 2]) }
+  x.report('C') { p bubble_sort_3([4, 3, 78, 2, 0, 2]) }
 end
